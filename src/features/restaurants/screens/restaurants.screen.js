944103a736d9759d571da8cd981923742f8b3c55
Shane Wilson <shanewilsonpro@gmail.com>
2021-05-15 16:14:07 -0700

    installed react navigation, added bottom tabs navigation and customized tab bar icons

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,15 +1,11 @@
 import React from "react";
 import { Searchbar } from "react-native-paper";
-import { StatusBar, FlatList, SafeAreaView } from "react-native";
+import { FlatList } from "react-native";
 import styled from "styled-components/native";
 
+import { SafeArea } from "../../../components/utility/safe-area.components";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.components";
 import { Spacer } from "../../../components/spacer/spacer.components";
-
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-`;
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
