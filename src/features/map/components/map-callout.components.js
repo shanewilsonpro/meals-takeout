a7ba5fb425997cd7ae05108bcc13ebeb60ef4a6f
Shane Wilson <shanewilsonpro@gmail.com>
2021-05-16 19:37:33 -0700

    installed mapview and webview, added map on screen with markers with callout restaurant name and picture

---
+++ src/features/map/components/map-callout.components.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { CompactRestaurantInfo } from "../../../components/restaurant/compact-restaurant-info.components";
+
+export const MapCallout = ({ restaurant }) => (
+  <CompactRestaurantInfo restaurant={restaurant} />
+);
