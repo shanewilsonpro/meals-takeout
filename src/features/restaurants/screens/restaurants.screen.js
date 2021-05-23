86efb26f51f689b48a48387db07e0c8c6ddae8d4
Shane Wilson <shanewilsonpro@gmail.com>
2021-05-22 20:52:35 -0700

    added settings navigation and screen, favourites list in settings and logout button

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,8 +1,9 @@
 import React, { useContext, useState } from "react";
-import { FlatList, TouchableOpacity } from "react-native";
+import { TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 import { ActivityIndicator, Colors } from "react-native-paper";
 
+import { FadeInView } from "../../../components/animations/fade.animation";
 import { SafeArea } from "../../../components/utility/safe-area.components";
 import { Spacer } from "../../../components/spacer/spacer.components";
 import { FavouritesBar } from "../../../components/favourites/favourites-bar.components";
@@ -13,11 +14,7 @@ import { FavouritesContext } from "../../../services/favourites/favourites.conte
 import { Search } from "../components/search.components";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.components";
 
-const RestaurantList = styled(FlatList).attrs({
-  contentContainerStyle: {
-    padding: 16,
-  },
-})``;
+import { RestaurantList } from "../components/restaurant-list.styles";
 
 const Loading = styled(ActivityIndicator)`
   margin-left: -25px;
@@ -62,7 +59,9 @@ export const RestaurantsScreen = ({ navigation }) => {
               }
             >
               <Spacer position="bottom" size="large">
-                <RestaurantInfoCard restaurant={item} />
+                <FadeInView>
+                  <RestaurantInfoCard restaurant={item} />
+                </FadeInView>
               </Spacer>
             </TouchableOpacity>
           );
